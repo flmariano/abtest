bf3ed6c2e43d9adf3ac8b38867ed4847ba078550
fmariano <florianmariano97@gmail.com>
2020-11-11 18:13:45 +0100

    basic project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'abtest1';
+export class AppComponent implements OnInit {
+  titles: string[] = [];
+  title: string;
+
+  // @ViewChild("button") button: ElementRef;
+
+  ngOnInit(): void {
+
+    this.titles[0] = 'A/B testing';
+    this.titles[1] = 'A/B test testing';
+
+    this.title = this.titles[Math.floor(Math.random() * 100) % 2];
+  }
 }
